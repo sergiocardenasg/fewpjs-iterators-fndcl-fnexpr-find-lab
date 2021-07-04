38a6cca51d3cab15817807c9433614a84b0238af
sergiocardenasg <sergiocardenas@utexas.edu>
2021-07-03 22:03:49 -0500

    commit tree 6ee29062818eaad0cfabaf93c6c46beda73592e4

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+function superbowlWin(results){
+    let win = results.find(results => results.result === "W");
+    return !!win ? win.year : undefined;
+};
 
+// superbowlWin = (record) => {
+//     let result = record.find( record => record.result === "W" )
+//     return !!result ? result.year : undefined
+//   }
